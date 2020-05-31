13c1238610acfd78424087de8c5d0169ff4e96f8
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2020-05-31 16:25:49 +0300

    Update kod.ino

--- Arduino/kod.ino
+++ Arduino/kod.ino
@@ -107,3 +107,79 @@ else{
 
  
 }
+int bluetoothkontrol(int btgelen){
+
+  
+     if(btgelen==65)
+    led1=1;
+    if(btgelen==66)
+    led1=0;
+
+     if(btgelen==67)
+    led2=1;
+    if(btgelen==68)
+    led2=0;
+
+     if(btgelen==69)
+    led3=1;
+    if(btgelen==70)
+    led3=0;
+
+     if(btgelen==71)
+    led4=1;
+    if(btgelen==72)
+    led4=0;
+
+     if(btgelen==73)
+    led5=1;
+    if(btgelen==74)
+    led5=0;
+
+     if(btgelen==75)
+    led6=1;
+    if(btgelen==76)
+    led6=0;
+
+    if(btgelen==77)
+    kapi=0;
+    if(btgelen==78)
+    kapi=45;
+
+    if(btgelen==80)
+    penc=45;
+    if(btgelen==82)
+    penc=0;
+    
+
+
+    
+    if(hareket()==1 && kapi==45 )
+    buzzer(50);
+    
+
+  
+
+     led(led1,led2,led3,led4,led5,led6);
+     kapiservo.write(kapi); 
+     delay(20);
+      pencereservo.write(penc); 
+    
+     delay(150  );
+ 
+}
+
+int led(byte a,byte b,byte c,byte d,byte e,byte f){
+  boolean dizi[] = {0, 0, f, e, d, c, b, a};  //nokta,g,f,e,d,c,b,a  = 1 acık 0 kapali
+  digitalWrite(latchPin, 0);       
+ 
+  for (int i = 0 ; i < 8 ; i++)     
+  {
+    x = dizi[i];                    //for dönügü boyunca dizinin i. değeri x'e atanır
+ 
+    digitalWrite(dataPin, x);       //datapin'e x değeri verilir
+    digitalWrite(clockPin, 1);      //saat darbesi ile yazma ve kaydırma işlemi yapılır
+    digitalWrite(clockPin, 0);
+  }
+  digitalWrite(latchPin, 1);        //8 bitli dizi çıkışa verilir
+}
+
